eef431e7b1248aae130a90a7e799e33434b106a0
Manoj Palkhade <manoj_palkhade@spe.sony.com>
2018-11-25 22:33:03 -0800

    test.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ echo "Installing serverless"
 echo "====================="
 npm install serverless
 
-echo "Deploying app to $env"
+echo "**********Deploying app to $env***********"
 echo "====================="
 serverless deploy --stage $env --package $CODEBUILD_SRC_DIR/artifacts/$env -v
