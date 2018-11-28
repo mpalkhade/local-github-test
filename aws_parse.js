99522645e1e12e10d4659c37d2bf29e2108e5dc9
Manoj Palkhade <manoj_palkhade@spe.sony.com>
2018-11-27 17:10:54 -0800

    test

--- aws_parse.js
+++ aws_parse.js
@@ -6,14 +6,19 @@ const writeCredentialExport = (pathToCredentials) => {
   let credJson = JSON.parse(data);
   let {
     SecretAccessKey: accessKey,
-    AccessKeyId: accessKeyId
+    AccessKeyId: accessKeyId,
+    SessionToken: sessionToken
   } = credJson.Credentials;
 
   let sw = fs.createWriteStream('export_secrets.sh');
   sw.once('open', () => {
    sw.write('#!\n');
    sw.write(`aws configure set aws_access_key_id ${accessKeyId};\n`);
+   sw.write(`export aws_access_key_id=${accessKeyId};\n`);
    sw.write(`aws configure set aws_secret_access_key ${accessKey};\n`);
+   sw.write(`export aws_secret_access_key=${accessKey};\n`);
+   sw.write(`aws configure set aws_session_token ${sessionToken};\n`);
+   sw.write(`export aws_session_token=${sessionToken};\n`);
    sw.write(`echo "configuration set!"`);
    sw.end();
    console.log('export_secrets created!');
